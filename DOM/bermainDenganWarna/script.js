0cff30e5e199b4b89d6c3b8a5c91c90057da5fc2
dandri <mdhandian@gmail.com>
2024-01-31 18:44:02 +0700

    update materi

--- DOM/bermainDenganWarna/script.js
+++ DOM/bermainDenganWarna/script.js
@@ -1,53 +1,51 @@
- // gak perlu panggil body juga boleh
- const body = document.getElementsByTagName('body')[0]
- const btn = document.querySelector('button');
- function ubahWarna(){
-     // body.style.backgroundColor = 'lightblue';
-     body.classList.toggle('biru-muda');
- }
- btn.onclick = ubahWarna;
+// ubah warna menjadi light blue mode
+const ubahWarna = document.getElementsByTagName('button')[0];
+const div = document.getElementById('satu');
+ubahWarna.addEventListener('click', function(){
+    document.body.classList.toggle('black');
+}) 
 
- // buat button acak warna
- const bAcakWarna = document.createElement('button');
- const tAcakWarna = document.createTextNode('Acak Warna');
- bAcakWarna.appendChild(tAcakWarna);
- bAcakWarna.setAttribute('type', 'button');
- btn.after(bAcakWarna);
- function acakWarna(){
-     const r = Math.round(Math.random() * 255 + 1);
-     const g = Math.round(Math.random() * 255 + 1);
-     const b = Math.round(Math.random() * 255 + 1);
-     body.style.backgroundColor = 'rgb('+ r +','+ g + ','+ b +')'
- }
- bAcakWarna.onclick = acakWarna;
+// membuat aksi acak warna dengan membuat button terlebih dahulu
+const btBaru = document.createElement('button');
+const teksBt = document.createTextNode('Acak Warna');
+btBaru.appendChild(teksBt);
+ubahWarna.after(btBaru)
+btBaru.addEventListener('click', function(){
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+})
 
- // buat slide warna
- const sMerah = document.querySelector('input[name=sMerah]');
- const sHijau = document.querySelector('input[name=sHijau]');
- const sBiru = document.querySelector('input[name=sBiru]');
+// buat aksi bar warna
+const sMerah = document.querySelector('input[name = sMerah]');
+const sHijau = document.querySelector('input[name = sHijau]');
+const sBiru = document.querySelector('input[name = sBiru]');
 
- sMerah.addEventListener('input', function(){
-     const r = sMerah.value;
-     const g = sHijau.value;
-     const b = sBiru.value;
-     document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
- });
- sHijau.addEventListener('input', function(){
-     const r = sMerah.value;
-     const g = sHijau.value;
-     const b = sBiru.value;
-     document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
- });
- sBiru.addEventListener('input', function(){
-     const r = sMerah.value;
-     const g = sHijau.value;
-     const b = sBiru.value;
-     document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
- });
+sMerah.addEventListener('input', function(){
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')';
+});
+sHijau.addEventListener('input', function(){
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')';
+});
+sBiru.addEventListener('input', function(){
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +', '+ g +', '+ b +')';
+});
+
 
  // buat warna sesuai perginya cursor
- document.body.addEventListener('mousemove', function(event){
+ const div3 = document.getElementById('tiga');
+ div3.addEventListener('mousemove', function(event){
      const xPos = Math.round((event.clientX / window.innerWidth) * 255);
      const yPos = Math.round((event.clientY / window.innerWidth) * 255);
-     document.body.style.backgroundColor = 'rgb('+ xPos +', '+ yPos +', 100)';
+     div3.style.backgroundColor = 'rgb('+ xPos +', '+ yPos +', 100)';
  });
--- DOM/domEvents.js
+++ DOM/domEvents.js
@@ -1,32 +1,53 @@
-// membuat aksi pada p2 menggunakan method onclick(event handler)
-const paragraf = document.querySelector('.p2');
+// eventHandler()
+
+// melakukan aksi pada saat klik p3 maka background akan berubah
+// panggil p3 
+const p3 = document.querySelector('.p3');
+// buat function untuk merubah warna
 function ubahWarna(){
-    paragraf.style.backgroundColor = 'coral'
+    p3.style.backgroundColor = 'lightblue';
 }
-paragraf.onclick = ubahWarna;
+// aksi agar berubah warna pada saat di click
+p3.onclick = ubahWarna;
 
-const judul = document.getElementById('judul');
-function cetakMiring(){
-    judul.style.fontStyle = 'italic'
+// merubah tulisan h1 pada saat di klik
+const h1 = document.getElementById('judul');
+function ubahTulisan(){
+    h1.innerHTML = 'Muhammad Dandri Handian';
+    h1.style.color = 'coral';
 }
-judul.onclick = cetakMiring;
+h1.onclick = ubahTulisan;
 
-// membuat aksi pada p4 menggunakan method addEventListener
+// addEventListener() (REKOMENDASI)
+/* Mouse Event = click, dblclick, mouseover, mouseenter,
+                mouseup, mouseleave, wheel, ..
+   Keyboard Event = keydown, keypress, keyup, ..
+   Form Event = reset, submit, ..
+*/
+// merubah background item 2 pada saat di click
+const li2 = document.querySelector('section#b ul li:nth-child(2)')
+li2.addEventListener('dblclick', function(){
+    li2.style.backgroundColor = 'green';
+})
+
+// menambah element li baru pada saat klik p4 dan mengganti tulisan p4
 const p4 = document.querySelector('section#b p');
-p4.addEventListener('dblclick', function(){
-    const ul = document.querySelector('section#b ul')
+p4.addEventListener('click', function(){
+    p4.innerHTML = "eits udah ditambahin tuh"
     const liBaru = document.createElement('li');
-    const teksLiBaru = document.createTextNode('Ini baru');
+    const teksLiBaru = document.createTextNode('ini baru');
     liBaru.appendChild(teksLiBaru);
+    const ul = document.querySelector('section#b ul');
     ul.appendChild(liBaru);
 })
 
-/* KEGUNAAN
-- Event handler akan menimpa aksi sebelumnya sehingga aksi yang dijalankan hanya aksi yang terakhir
-- addEventListener akan menjalankan semua aksi yang dituliskan
-
-MOUSE EVENT
-click, dblclick, mouseover, mouseenter, mouseup, wheel, ..
-*/
-
-
+// melakukan aksi pada p1 ketika mouse masuk bgnya grey, ketika keluar lagi white
+const p1 = document.getElementsByClassName('p1')[0];
+p1.addEventListener('mouseenter', function(){
+    p1.style.backgroundColor = 'grey';
+    p1.innerHTML = 'Hallo nama saya Muhammad Dandri Handian, saya lulusan s1 jurusan teknik informatika dari kampus Universitas Komputer Indonesia'
+});
+p1.addEventListener('mouseleave', function(){
+    p1.style.backgroundColor = 'white';
+    p1.innerHTML = 'paragraf 1';
+})
